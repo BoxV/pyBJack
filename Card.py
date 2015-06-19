d91cb719c7fbedbf9db39215edede9d92fbee5fe
BoxV <JTang@jonathans-mbp.gateway.sonic.net>
2015-06-18 21:44:49 -0700

    Kept on writing more stuff

--- Card.py
+++ Card.py
@@ -1,23 +1,29 @@
 class Cards:
-	names = {1: "A", 11: "J", 12: "Q", 13: "K"}
+  names = {1: "A", 11: "J", 12: "Q", 13: "K"}
 
-	def __init__ (self, value):
-		self.value = value
-		if (value > 10):
-			self.isFace = True
-		else:
-			self.isFace = False
-	
-	def getName (self):
-		if (self.value in Cards.names):
-			return Cards.names[self.value]
-		else:
-			return str(self.value)
+  def __init__ (self, value):
+    self.value = value
+    if (value > 10):
+      self.isFace = True
+    else:
+      self.isFace = False
+  
+  def getName (self):
+    if (self.value in Cards.names):
+      return Cards.names[self.value]
+    else:
+      return str(self.value)
 
-	def getPoints (self):
-		if (self.isFace):
-			return 10
-		elif (self.value == 1):
-			return 11
-		else:
-			return self.value
+  def getPoints (self):
+    if (self.isFace):
+      return 10
+    elif (self.value == 1):
+      return 11
+    else:
+      return self.value
+
+  def __str__(self):
+    return Cards.getName(self)
+
+  def __repr__(self):
+    return str(self)
--- blackJack.py
+++ blackJack.py
@@ -1,21 +1,21 @@
 from Card import *
 import random
 
-#start 2 random num, 1-10 
-#
-#
+Hand = []
+
+def Hit ():
+  Hand.append(Cards(random.randint(1, 13)))
+
 
 print "Hi! This is a game of Black Jack."
 
-Hand = []
-
-Hand.append(Cards(random.randint(1, 13)))
-Hand.append(Cards(random.randint(1, 13)))
+Hit()
+Hit()
 
 total = 0
 for card in Hand:
-	print card.getName()
-	total += card.getPoints()
+  print card.getName()
+  total += card.getPoints()
 
 print "Your current total is: %d" % (total)
 print "If you would like to hit, type \"hit\"."
@@ -23,6 +23,5 @@ print "If you would like to stand, type \"stand\"."
 choice = raw_input("").lower()
 
 if (choice == "hit"):
-	add = Hand.append(Cards(random.randint(1, 13)))
-	total += add
-	print total
+  Hit()
+  print Hand[2]
