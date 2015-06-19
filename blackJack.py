c51f892966a2b2201768a4dc3a3dc33bd66d817a
BoxV <JTang@jonathans-mbp.gateway.sonic.net>
2015-06-18 22:20:18 -0700

    Update 3
    Now possible to lose or win.
    Will continue until either options happen, or you stand.

--- blackJack.py
+++ blackJack.py
@@ -17,11 +17,51 @@ for card in Hand:
   print card.getName()
   total += card.getPoints()
 
-print "Your current total is: %d" % (total)
-print "If you would like to hit, type \"hit\"."
-print "If you would like to stand, type \"stand\"."
-choice = raw_input("").lower()
+#print "Your current total is: %d" % (total)
+#print "If you would like to hit, type \"hit\"."
+#print "If you would like to stand, type \"stand\"."
+#choice = raw_input("").lower()
 
-if (choice == "hit"):
-  Hit()
-  print Hand[2]
+count = 2
+for count in range(2, 11):
+  
+  if (total == 21):
+    print "You win!1!"
+    break
+
+  elif (total > 21):
+    print "You lose :((((("
+    break
+
+  else:
+
+    print "If you would like to hit, type \"hit\"."
+    print "If you would like to stand, type \"stand\"."
+    choice = raw_input("").lower()
+
+    if (choice == "hit"):
+      Hit()
+      total += Hand[count].getPoints()
+      print Hand[count]
+      print total
+
+    elif (choice == "stand"):
+      break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
